4bdd263b6ba206f59b461cc62f3b20abb3f3eff9
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-10-27 12:23:14 +0530

    Improved code and added submit Form service

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable @typescript-eslint/no-use-before-define */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable arrow-body-style */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
 import {
@@ -12,116 +11,24 @@ import {
 } from 'antd';
 import './question.scss';
 import myForm from './myForm.json';
+import { QuestionsFormProp } from './util';
+import submitQuestionsForm from '../../services/submitForm';
 
-const QuestionsForm = (): JSX.Element => {
-  const [budget, setBudget] = useState({ min: 20000, max: 50000 });
-
-  const onFormSumbmit = (values: any) => {
-    const result = {
-      ...values,
-      artist_budget: budget,
-      target_audience: {
-        age_group: values.age,
-        gender: values.gender,
-        genre: values.gender,
-      },
-    };
-    const deleteProp: Array<string> = ['age', 'gender', 'genre'];
-    deleteProp.map((prop: string) => {
-      return delete result[prop];
-    });
-  };
-
-  function onBudgetChange(event: any) {
-    setBudget({ min: event[0], max: event[1] });
-  }
-  return (
-    <div className="questions-container">
-      <Form
-        id="qustionsForm"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-        initialValues={{ size: 1000 }}
-        onFinish={onFormSumbmit}
-      >
-        { myForm && renderFormFields(myForm, budget, onBudgetChange) }
-        <Form.Item name="sponsorship_type" label="Type of sponsorship sought">
-          <Select>
-            <Select.Option value="brand awareness">Brand awareness</Select.Option>
-            <Select.Option value="direct sales">Direct sales</Select.Option>
-            <Select.Option value="customer engagement">Customer Engagement</Select.Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item name="wanted_brands" label="Specific brand you want">
-          <Select
-            showSearch
-            mode="multiple"
-          >
-            <Select.Option value="delhi">Coca Cola</Select.Option>
-            <Select.Option value="mumbai">Moutain deo</Select.Option>
-            <Select.Option value="london">Kingfisher</Select.Option>
-            <Select.Option value="kolkata">Vimal</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item name="unwanted_brands" label="Brand you don't want">
-          <Select
-            showSearch
-            mode="multiple"
-          >
-            <Select.Option value="delhi">Coca Cola</Select.Option>
-            <Select.Option value="mumbai">Moutain deo</Select.Option>
-            <Select.Option value="london">Kingfisher</Select.Option>
-            <Select.Option value="kolkata">Vimal</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item name="age" label="Targeted age group">
-          <Select
-            showSearch
-            mode="multiple"
-          >
-            <Select.Option value="18-25">18 - 25</Select.Option>
-            <Select.Option value="26-35">26 - 35</Select.Option>
-            <Select.Option value="36-60">36 - 60</Select.Option>
-            <Select.Option value="60+">60 plus</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item name="gender" label="Targeted gender">
-          <Select
-            showSearch
-          >
-            <Select.Option value="male">Male</Select.Option>
-            <Select.Option value="female">Female</Select.Option>
-            <Select.Option value="both">Both</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item name="genre" label="Targeted genre">
-          <Select
-            showSearch
-          >
-            <Select.Option value="male">Hollywood</Select.Option>
-            <Select.Option value="female">POP</Select.Option>
-            <Select.Option value="both">Rock</Select.Option>
-            <Select.Option value="both">Classic</Select.Option>
-          </Select>
-        </Form.Item>
-        <Button htmlType="submit">Submit</Button>
-      </Form>
-    </div>
-  );
-};
-
-function renderOptions(options: any) {
+const renderOptions = (options: any) => {
   return options.map((option: string) => {
     return <Select.Option key={`option${option}`} value={option}>{option}</Select.Option>;
   });
-}
+};
 
-function renderSelect(field: any) {
+const renderSelect = (field: any) => {
   if (field.multiple) {
     return (
-      <Form.Item key={`field${field.name}`} name={field.name} label={field.label}>
+      <Form.Item
+        key={`field${field.name}`}
+        name={field.name}
+        label={field.label}
+        rules={[{ required: field.required || false, message: field.message || 'Not required' }]}
+      >
         <Select
           showSearch
           mode="multiple"
@@ -132,7 +39,12 @@ function renderSelect(field: any) {
     );
   }
   return (
-    <Form.Item key={`field${field.name}`} name={field.name} label={field.label}>
+    <Form.Item
+      key={`field${field.name}`}
+      name={field.name}
+      label={field.label}
+      rules={[{ required: field.required || true, message: field.message || 'Not required' }]}
+    >
       <Select
         showSearch
       >
@@ -140,26 +52,15 @@ function renderSelect(field: any) {
       </Select>
     </Form.Item>
   );
-}
+};
 
-function renderFormFields(formData: any, budget: any, onBudgetChange:any) {
-  return formData.map((field: any) => {
-    switch (field.type) {
-      case 'select':
-        return renderSelect(field);
-
-      case 'slider':
-        return renderSlider(field, budget, onBudgetChange);
-
-      default:
-        return <span>None</span>;
-    }
-  });
-}
-
-function renderSlider(field: any, budget: any, onBudgetChange: any): any {
+const renderSlider = (field: any, budget: any, onBudgetChange: any): any => {
   return (
-    <Form.Item key={`field${field.name}`} name={field.name} label={field.label}>
+    <Form.Item
+      key={`field${field.name}`}
+      name={field.name}
+      label={field.label}
+    >
       <Slider
         range
         min={field.min}
@@ -183,6 +84,62 @@ function renderSlider(field: any, budget: any, onBudgetChange: any): any {
       </span>
     </Form.Item>
   );
-}
+};
+
+const renderFormFields = (formData: any, budget: any, onBudgetChange:any) => {
+  return formData.map((field: any) => {
+    switch (field.type) {
+      case 'select':
+        return renderSelect(field);
+
+      case 'slider':
+        return renderSlider(field, budget, onBudgetChange);
+
+      default:
+        return <span>None</span>;
+    }
+  });
+};
+
+const QuestionsForm = ({ setVisible } : QuestionsFormProp): JSX.Element => {
+  const [budget, setBudget] = useState({ min: 20000, max: 50000 });
+
+  const onFormSumbmit = (values: any) => {
+    const result = {
+      ...values,
+      artist_budget: budget,
+      target_audience: {
+        age_group: values.age,
+        gender: values.gender,
+        genre: values.genre,
+      },
+    };
+    const deleteProp: Array<string> = ['age', 'gender', 'genre'];
+    deleteProp.map((prop: string) => {
+      return delete result[prop];
+    });
+    submitQuestionsForm(result);
+    setVisible(false);
+  };
+
+  const onBudgetChange = (event: any) => {
+    setBudget({ min: event[0], max: event[1] });
+  };
+  return (
+    <div className="questions-container">
+      <Form
+        id="qustionsForm"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        initialValues={{ size: 1000 }}
+        onFinish={onFormSumbmit}
+      >
+        { myForm && renderFormFields(myForm, budget, onBudgetChange) }
+        <Button htmlType="submit">Submit</Button>
+      </Form>
+    </div>
+  );
+};
 
 export default QuestionsForm;
--- src/services/getRecommendedArtists.tsx
+++ src/services/getRecommendedArtists.tsx
@@ -3,7 +3,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 
-const baseURL = 'http://52.66.164.23:3000';
+import config from '../config/backend.json';
+
+const { baseURL } = config;
 
 const mockData = [
   {
