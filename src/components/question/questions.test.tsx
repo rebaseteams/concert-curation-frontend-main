cc8de2d3a6abfd5a096440b4bf2e72911e2419ef
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-10-27 15:42:12 +0530

    added package-lock in git ignore

--- src/components/question/questions.test.tsx
+++ src/components/question/questions.test.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import QuestionsForm from './index';
 
-describe('Questions Componet', () => {
+describe('Questions Component', () => {
   beforeEach(() => {
     Object.defineProperty(window, 'matchMedia', {
       writable: true,
