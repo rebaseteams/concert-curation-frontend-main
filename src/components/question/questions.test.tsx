2dfebce927c3a26f4118a799192457ccdef94730
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-10-27 15:14:57 +0530

    Added test for questions form

---
+++ src/components/question/questions.test.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import QuestionsForm from './index';
+
+describe('Questions Componet', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+  test('should render Questions component', () => {
+    render(<QuestionsForm setVisible />);
+    expect(screen.getByText('Which type of event is it?')).toBeInTheDocument();
+  });
+  test('should render budget field', () => {
+    render(<QuestionsForm setVisible />);
+    expect(screen.getByText('Set budget')).toBeInTheDocument();
+  });
+});
