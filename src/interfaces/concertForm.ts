1ec95ea4cf01a7cd9df0d4f85f006f295ae28a03
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-12 11:35:11 +0530

    Error handling for no server available, Concert not created and loading feature added Code modulation for createConcertFormData

---
+++ src/interfaces/concertForm.ts
@@ -0,0 +1,12 @@
+export type onSubmitFormDataType = {
+  age: Array<string>;
+  artistBudget: undefined;
+  concertName: string;
+  eventType: string;
+  gender: Array<string>;
+  genre: Array<string>;
+  sponsorshipType: string;
+  unwantedBrands: Array<string>;
+  venue: Array<string>;
+  wantedBrands: Array<string>;
+}
