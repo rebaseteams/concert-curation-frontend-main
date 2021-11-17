9b025b24eece3b32e2c8bf943e2a9320c528f479
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-17 15:35:57 +0530

    Dark theme enabled

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.dark.css';
 
 ReactDOM.render(
   <App />,
