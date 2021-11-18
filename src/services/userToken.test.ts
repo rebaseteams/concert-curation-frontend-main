f2feb928ac15c2cdd634dbecf32db5ab30d3d3a6
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-18 12:47:30 +0530

    Test Added for mock localstorage

---
+++ src/services/userToken.test.ts
@@ -0,0 +1,16 @@
+import mockLocalStorage from '../testUtils/mockLocalstorage';
+import extractUserToken from './userToken';
+
+const { getItemMock } = mockLocalStorage();
+
+describe('User Token From Localstorage', () => {
+  it('Should return user tocker if localstorage have userid', () => {
+    getItemMock.mockReturnValue('1238989');
+    expect(extractUserToken()).toEqual('1238989');
+  });
+
+  it('Should return "" empty if localstorage do not have userid', () => {
+    getItemMock.mockReturnValue(null);
+    expect(extractUserToken()).toEqual('');
+  });
+});
