f1a3b83b935718338c877a2bddd4cba9e4f7dbc0
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-22 18:39:01 +0530

    Changes

--- src/dataLayer/repositories/http/artistRecommendation.ts
+++ src/dataLayer/repositories/http/artistRecommendation.ts
@@ -4,6 +4,7 @@ import ArtistRecommendationInterface from '../../../model/interfaces/artistRecom
 import config from '../../../config';
 import { PatchRequest } from '../../../model/types/patch-request';
 import { QuestionsUI } from '../../../model/types/questions';
+import { ServiceResponse } from '../../../model/types/service-response';
 
 const server = config.PROD_SERVER;
 export default class ArtistRecommendationRepo implements ArtistRecommendationInterface {
@@ -13,18 +14,15 @@ export default class ArtistRecommendationRepo implements ArtistRecommendationInt
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  getAllRecommendations = async (): Promise<any> => {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const response = await axios.get(`${server}/api/artists/recommendations/`);
-      if (response.status !== 200) {
-        return { error: true, status: response.status, message: 'unknown error' };
+  getAllRecommendations = async (): Promise<ServiceResponse> => new Promise((resolve) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    axios.get(`${server}/api/artists/recommendations/`).then((val: any) => {
+      if (val.status !== 200) {
+        resolve({ error: true, status: val.status, message: 'unknown error' });
       }
-      return { error: false, data: response.data, message: 'success' };
-    } catch (error) {
-      return { error: true, status: 501, message: 'Unknown' };
-    }
-  };
+      resolve({ error: false, data: val.data, message: 'success' });
+    });
+  });
 
   getRecommendation = async (recommendationId : string) => {
     const resp = await axios.get(`${server}/api/artists/recommendations/${recommendationId}`);
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.dark.css';
-import config from './config';
+// import config from './config';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={config.AUTH_DOMAIN}
-    clientId={config.AUTH_CLIENT_ID}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  // <Auth0Provider
+  //   domain={config.AUTH_DOMAIN}
+  //   clientId={config.AUTH_CLIENT_ID}
+  //   redirectUri={window.location.origin}
+  // >
+  <App />,
+  // </Auth0Provider>,
   document.getElementById('root'),
 );
 
