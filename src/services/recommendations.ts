8dc5afd868c0e57573a13a7ade703b5adc0c74bc
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-11-29 11:46:36 +0530

    Removed old addRecommendation service

--- src/services/recommendations.ts
+++ src/services/recommendations.ts
@@ -30,30 +30,6 @@ export const getRecommendedArtists = async (formId: string): Promise<any> => {
   }
 };
 
-export const submitQuestionsForm = async (questions: QuestionsUI): Promise<any> => {
-  // Insuring data should not have any other fields exept specified below
-  const data = _.pick(questions, ['userId',
-    'concertName',
-    'eventType',
-    'venue',
-    'artistBudget',
-    'sponsorshipType',
-    'wantedBrands',
-    'unwantedBrands',
-    'targetAudience',
-    'whatSellsMost',
-  ]);
-  try {
-    const response = await axios.post(`${baseURL}/artists/recommendations`, data);
-    if (response.status === 200) {
-      return { error: false, data: response.data };
-    }
-    return { error: true, message: response.statusText, status: response.status };
-  } catch (error) {
-    return { error: true, message: error, status: 500 };
-  }
-};
-
 export const deleteRecommendation = async (formId: string): Promise<any> => {
   try {
     const response = await axios.delete(`${baseURL}/artists/recommendations/${formId}`);
