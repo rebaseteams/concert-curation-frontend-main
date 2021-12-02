a7a07f8dcc8100a3318976090ab3dae6497a0b67
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-12-02 14:55:56 +0530

    delete document

--- src/model/interfaces/documents.ts
+++ src/model/interfaces/documents.ts
@@ -1,9 +1,10 @@
 /* eslint-disable max-len */
 /* eslint-disable semi */
 import { CreateDocumentForm } from '../types/collaborationForm';
-import { CreateDocumentResponse, GetDocumentsResponse } from '../types/service-response';
+import { CreateDocumentResponse, DeleteDocumentResponse, GetDocumentsResponse } from '../types/service-response';
 
 export default interface DocumentsInterface {
   createDocument (createDocumentForm: CreateDocumentForm): Promise<CreateDocumentResponse>;
   getDocuments (): Promise<GetDocumentsResponse>;
+  deleteDocument (documentId: string): Promise<DeleteDocumentResponse>;
 }
