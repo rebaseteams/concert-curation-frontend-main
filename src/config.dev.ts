e2984282ac02f35446156ab0986ec4228baef403
Sagar Gudka <sagar@ubuntu.com>
2021-12-08 19:32:37 +0530

    Remove service dependecy from useGetConcert

--- src/config.dev.ts
+++ src/config.dev.ts
@@ -1,12 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import { UnsignedTokenService } from './adapters/token.service.unsigned';
 import { InMemoryAuth0Provider } from './contexts/auth-context.in-memory';
+import ArtistRecommendationRepo from './dataLayer/repositories/http/artistRecommendation';
+import ArtistRecommendation from './dataLayer/services/artistRecommendation';
 import { Config } from './model/types/config';
 
 const tokenService = new UnsignedTokenService();
 
 export const config: Config = {
   mode: 'development',
+  services: {
+    artistRecommendation: new ArtistRecommendation(new ArtistRecommendationRepo()),
+  },
   providers: {
     Auth: ({ children }) => InMemoryAuth0Provider({
       children,
