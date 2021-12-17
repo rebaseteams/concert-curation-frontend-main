29b97bc7c1c1cc05ba06665b703b45dbed65af58
Praveen Prajapti <praveen.prajapati.rebase@gmail.com>
2021-12-17 13:31:00 +0530

    Changes content

--- src/visualLayer/pages/landing/branding.tsx
+++ src/visualLayer/pages/landing/branding.tsx
@@ -4,10 +4,10 @@ const Branding = (): JSX.Element => (
   <div className="height-100 column-flex justify-center align-center first-sec">
     <h4>Cuttime</h4>
     <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Odit ab voluptate assumenda maiores cumque itaque nostrum,
-      expedita amet ratione neque sequi nisi illum vel explicabo animi eius,
-      ut atque. Similique.
+      Music and Brands
+    </p>
+    <p>
+      Quantified and connected
     </p>
     <Link className="button-text" to="dashboard">
       Dashboard
