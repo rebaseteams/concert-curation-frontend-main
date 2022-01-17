58af1a68827938b51ba1e563e7fc86148d900c12
prasanarebase <92080819+prasanarebase@users.noreply.github.com>
2022-01-17 10:34:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+
 import axios, { AxiosRequestConfig } from 'axios';
 import {
   BrowserRouter, Link, Route, Routes, Navigate,
