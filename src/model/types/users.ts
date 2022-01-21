d9cb2f4930ef24379541ab426dce615594da9ddd
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-21 16:37:31 +0530

    fixed usersUri

---
+++ src/model/types/users.ts
@@ -0,0 +1,24 @@
+export type CreateUserForm = {
+  name: string,
+  email: string,
+  password: string,
+  role: string,
+};
+
+export type NewUserResponseData = {
+    id: string,
+    name: string,
+    email: string,
+    approved: boolean,
+    roles: Array<string>,
+}
+
+export type ApproveUserForm = {
+    id: string,
+    approval: boolean,
+}
+
+export type UpdateUsersRoleForm = {
+    id: string,
+    roles: Array<string>,
+}
