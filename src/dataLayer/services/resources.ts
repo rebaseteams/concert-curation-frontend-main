321d5f36fff730d73be868e13a7714eec999de40
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-24 12:27:49 +0530

    created-resources-service

---
+++ src/dataLayer/services/resources.ts
@@ -0,0 +1,57 @@
+import { ResourcesInterface } from '../../model/interfaces/resources';
+import { CreateResourceForm, EditResourceForm } from '../../model/types/resources';
+import {
+  CreateResourceResponse,
+  DeleteResourceResponse,
+  EditResourceResponse,
+  GetResourceByIdResponse,
+  GetResourcesResponse,
+} from '../../model/types/service-response';
+
+export default class Resources implements ResourcesInterface {
+    private resourcesRepo: ResourcesInterface;
+
+    constructor(resourcesRepo: ResourcesInterface) {
+      this.resourcesRepo = resourcesRepo;
+    }
+
+    createResource(createResourceData : CreateResourceForm): Promise<CreateResourceResponse> {
+      return new Promise((resolve) => {
+        this.resourcesRepo.createResource(createResourceData).then((response) => {
+          resolve(response);
+        });
+      });
+    }
+
+    getResourceById(ResourceId: string): Promise<GetResourceByIdResponse> {
+      return new Promise((resolve) => {
+        this.resourcesRepo.getResourceById(ResourceId).then((response) => {
+          resolve(response);
+        });
+      });
+    }
+
+    getResources(skip: number, limit: number): Promise<GetResourcesResponse> {
+      return new Promise((resolve) => {
+        this.resourcesRepo.getResources(skip, limit).then((response) => {
+          resolve(response);
+        });
+      });
+    }
+
+    editResource(editResourceData: EditResourceForm): Promise<EditResourceResponse> {
+      return new Promise((resolve) => {
+        this.resourcesRepo.editResource(editResourceData).then((response) => {
+          resolve(response);
+        });
+      });
+    }
+
+    deleteResource(ResourceId: string): Promise<DeleteResourceResponse> {
+      return new Promise((resolve) => {
+        this.resourcesRepo.deleteResource(ResourceId).then((response) => {
+          resolve(response);
+        });
+      });
+    }
+}
