6f80479a716380473011b3787b857dce53d85a38
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-27 15:24:02 +0530

    create role from modal

--- src/visualLayer/pages/superadmin/role/index.tsx
+++ src/visualLayer/pages/superadmin/role/index.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-template-curly-in-string */
 /* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/no-use-before-define */
 
 // TODO : Replace hardcoded data with API data
 
@@ -16,16 +17,6 @@ import CustomModal from '../../../components/CustomModal';
 import IconRenderer from '../../../components/IconRenderer';
 
 const list : Array<{id: string, name : string, resource : Array<{id: string, permission : boolean, name : string, actions : string}>, actions : Array<string>}> = [];
-const resources = [
-  {
-    name: 'res1',
-    actions: ['ac1', 'ac2', 'ac3'],
-  },
-  {
-    name: 'res2',
-    actions: ['ac21', 'ac22', 'ac23'],
-  },
-];
 
 type RolesProps = {
   rolesService: RolesInterface;
@@ -36,21 +27,31 @@ const Roles = ({ rolesService, resourcesService }: RolesProps) : JSX.Element => 
   const [role, setRole] = useState<{name : string, resource : Array<{permission : boolean, name : string, actions : string}>, actions : Array<string>}>({ name: '', resource: [], actions: [] });
   const [listToDisplay, setListToDisplay] = useState<Array<{id: string, name : string, resource : Array<{permission : boolean, name : string, actions : string}>, actions : Array<string>}>>(list);
   const [actions, setActions] = useState<Array<Array<string>>>([]);
+  const [resources, setResources] = useState<Array<{id: string, name: string, actions: Array<string>}>>([]);
+
+  const [createForm] = Form.useForm();
 
   const loadRoles = async () => {
     const roles = await rolesService.getRoles(0, 10);
+    console.log(roles);
     if (roles.success) {
       roles.data.roles.forEach((item) => {
         list.push({
           id: item.id,
           name: item.name,
-          resource: item.resource_actions.actions.map((i) => ({
-            id: item.resource_actions.resourceId,
-            permission: i.permission,
-            name: item.name,
-            actions: i.name,
-          })),
-          actions: item.resource_actions.actions.map((ac) => ac.name),
+          resource: [{
+            id: 'dd',
+            permission: true,
+            name: 'dd',
+            actions: 'dd',
+          }],
+          // resource: item.resource_actions.actions.map((i) => ({
+          //   id: item.resource_actions.resourceId,
+          //   permission: i.permission,
+          //   name: item.name,
+          //   actions: i.name,
+          // })),
+          actions: ['ss'],
         });
       });
       setListToDisplay(list);
@@ -58,9 +59,13 @@ const Roles = ({ rolesService, resourcesService }: RolesProps) : JSX.Element => 
     setLoadingRoles(false);
   };
 
+  console.log(actions);
+  // console.log(listToDisplay);
+
   const loadResources = async () => {
     const fetchedresources = await resourcesService.getResources(0, 10);
     console.log(fetchedresources);
+    setResources(fetchedresources.data.resources);
   };
 
   useEffect(() => {
@@ -72,25 +77,47 @@ const Roles = ({ rolesService, resourcesService }: RolesProps) : JSX.Element => 
     required: '${label} is required!',
   };
 
-  const onValuesChange = async (changedValues: {name: string, actions: Array<string>}, values: {name: string, actions: Array<string>}) => {
+  const onValuesChange = async (
+    changedValues: {name: string, actions: Array<string>},
+    values: {name: string, resource : Array<{permission : boolean, name : string, actions : string}>, actions: Array<string>},
+  ) => {
     console.log(values);
+    setRole(values);
   };
 
-  const onSave = () => {
-    console.log(role);
+  type formType = {name : string, resource : Array<{permission : boolean, name : string, actions : string}>}
+
+  const onSave = async (value: formType) => {
+    console.log(value);
+    const array = resources.slice().filter((i) => value.resource.some(({ name }) => i.name === name));
+    const newArr = array.map((res) => {
+      const resArray = value.resource.filter((i) => i.name === res.name);
+      return {
+        resourceId: res.id,
+        actions: resArray.map((i) => ({
+          name: i.name,
+          permission: i.permission,
+        })),
+      };
+    });
+    rolesService.createRole({
+      name: value.name,
+      resourceActions: newArr,
+    });
+    modal.hideModal();
   };
 
   const modal = CustomModal(
     'Create Role',
     'Save',
     'Cancel',
-    onSave,
+    createForm.submit,
     <>
-      <Form onValuesChange={onValuesChange} validateMessages={validateMessages}>
-        <Form.Item name={['role', 'name']} label="Name" rules={[{ required: true }]}>
+      <Form form={createForm} onFinish={onSave} onValuesChange={onValuesChange} validateMessages={validateMessages}>
+        <Form.Item name={['name']} label="Name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
-        <Form.List name={['role', 'resource']}>
+        <Form.List name={['resource']}>
           {(fields, { add, remove }) => (
             <>
               {fields.map(({ key, name, ...restField }) => (
@@ -108,6 +135,7 @@ const Roles = ({ rolesService, resourcesService }: RolesProps) : JSX.Element => 
                             ac[key] = element.actions;
                             setActions(ac);
                           }
+                          console.log('element=', element);
                         });
                       }}
                       placeholder="Select Resource"
@@ -117,6 +145,7 @@ const Roles = ({ rolesService, resourcesService }: RolesProps) : JSX.Element => 
                       ))}
                     </Select>
                   </Form.Item>
+                  <Form.Item name={[name, 'id']} required />
                   <Form.Item
                     {...restField}
                     name={[name, 'actions']}
