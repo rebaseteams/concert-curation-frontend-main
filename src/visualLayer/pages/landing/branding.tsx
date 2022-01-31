ac2764a390cbbaa9427e63e63de52b2a2aa67fc7
saritChauhan <saritsinghchauhan@gmail.com>
2022-01-31 11:07:21 +0530

    superadmin access

--- src/visualLayer/pages/landing/branding.tsx
+++ src/visualLayer/pages/landing/branding.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import checkResourceAction from '../../../utils/checkResourceAction';
 
 const Branding = (): JSX.Element => (
   <div className="height-100 column-flex justify-center align-center first-sec">
@@ -12,6 +13,16 @@ const Branding = (): JSX.Element => (
     <Link className="button-text" to="dashboard">
       Dashboard
     </Link>
+    {
+      checkResourceAction(
+        'superadmin dashboard',
+        'view',
+        <Link className="button-text" to="superadmin/dashboard">
+          SuperAdmin Dashboard
+        </Link>,
+      )
+    }
+
   </div>
 );
 
