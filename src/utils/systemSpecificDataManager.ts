a51b9be1fd07036981a32b316d3a17907382e226
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-02-24 12:56:00 +0530

    Created system specific data util

---
+++ src/utils/systemSpecificDataManager.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+type SsdKeys = 'actions' | 'allRoles' | 'roles';
+
+export function setSsd(name: SsdKeys, value: any): void {
+  const ssv = localStorage.getItem('ssv');
+  const ssvObj = ssv ? JSON.parse(ssv) : {};
+  ssvObj[name] = value;
+  localStorage.setItem('ssv', JSON.stringify(ssvObj));
+}
+
+export function getSsd(name: SsdKeys): any {
+  const ssv = localStorage.getItem('ssv');
+  const ssvObj = ssv ? JSON.parse(ssv) : {};
+  if (ssvObj[name]) return ssvObj[name];
+  return null;
+}
+
+export function removeSsd(name: SsdKeys): void {
+  const ssv = localStorage.getItem('ssv');
+  const ssvObj = ssv ? JSON.parse(ssv) : {};
+  if (ssvObj[name]) delete ssvObj[name];
+  localStorage.setItem('ssv', JSON.stringify(ssvObj));
+}
