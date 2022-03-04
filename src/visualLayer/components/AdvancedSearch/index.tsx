b5654cb6ef3b21318750b58e9ae1bb1030e9f5c5
saritChauhan <saritsinghchauhan@gmail.com>
2022-03-04 16:21:52 +0530

    fixed some bugs and added sample data

--- src/visualLayer/components/AdvancedSearch/index.tsx
+++ src/visualLayer/components/AdvancedSearch/index.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Select } from 'antd';
+/* eslint-disable no-console */
+import {
+  Button, notification,
+} from 'antd';
 import { useEffect, useState } from 'react';
-import { AdvancedSearchProps, CategoryObject, ValueObject } from './types';
+import { SearchOutlined } from '@ant-design/icons';
+import {
+  AdvancedSearchProps, CategoryObject, OptionType, ValueObject,
+} from './types';
+
+import './styles.scss';
+import SearchPopup from './popup';
+import { metaTypes } from './utils';
 
 const AdvancedSearch = ({
   filterOptions,
@@ -10,96 +20,112 @@ const AdvancedSearch = ({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [options, setOptions] = useState<Array<any>>([]);
   const [category, setCategory] = useState<string>('');
+  const [subcategories, setSubcategories] = useState<Array<string>>([]);
   const [subcategory, setSubcategory] = useState<string>('');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
 
   const renderItem = (title: string, type: string) => ({
-    value: `${type}-${title}`,
+    type,
+    value: title,
     label: <>{title}</>,
   });
 
-  const decryptValue = (value: string): ValueObject => {
-    const arr = value.split('-');
-    return {
-      type: arr[0],
-      value: arr[1],
-    };
-  };
-
   const metaData = [
     {
-      type: 'category',
-      label: "I'm looking for",
+      type: metaTypes.category,
+      label: "I'm looking for...",
     },
     {
-      type: 'subcategory',
+      type: metaTypes.subcategory,
       label: 'Narrow your search',
     },
     {
-      type: 'recent-searches',
+      type: metaTypes.recent,
       label: 'Recent Searches',
     },
     {
-      type: 'results',
+      type: metaTypes.results,
       label: 'fetched results',
     },
 
   ];
 
   const getMetaOptions = (sc: Array<string>) => metaData.map((i) => {
-    if (i.type === 'category') {
+    if (i.type === metaTypes.category) {
       return {
         type: i.type,
-        label: <div>{i.label}</div>,
+        label: i.label,
+        viewOption: 'grid',
         options: filterOptions.map((c) => renderItem(c.category, i.type)),
       };
     }
-    if (i.type === 'subcategory') {
+    if (i.type === metaTypes.subcategory) {
       return {
         type: i.type,
-        label: <div>{i.label}</div>,
+        label: i.label,
         options: sc.map((s) => renderItem(s, i.type)),
+      };
+    }
+    if (i.type === metaTypes.recent) {
+      return {
+        type: i.type,
+        label: i.label,
+        options: [renderItem('recent1', i.type), renderItem('recent2', i.type)],
       };
     }
     return {
       type: i.type,
-      label: <div>{i.label}</div>,
+      label: i.label,
       options: [],
     };
   });
 
   const setSelectOptions = (type: string, subcatgs: Array<string>) => {
     const op = getMetaOptions(subcatgs);
-    const fil = op.filter((i) => i.type === type || i.type === 'recent-searches');
+    const fil = op.filter((i) => i.type === type || i.type === metaTypes.recent);
     setOptions(fil);
   };
 
-  const handleChange = (value: Array<string>) => {
-    const valueArray = value.map((v) => decryptValue(v));
-
-    const hasCategory = !!valueArray.find((i) => i.type === 'category');
-    const hasSubcategory = !!valueArray.find((i) => i.type === 'subcategory');
-    const hasRecentsearches = !!valueArray.find((i) => i.type === 'recent-searches');
-    const hasResults = !!valueArray.find((i) => i.type === 'results');
+  const onTagChange = (valueArray: Array<ValueObject>) => {
+    const hasCategory = !!valueArray.find((i) => i.type === metaTypes.category);
+    const hasSubcategory = !!valueArray.find((i) => i.type === metaTypes.subcategory);
+    const hasRecentSearches = !!valueArray.find((i) => i.type === metaTypes.recent);
+    const hasResults = !!valueArray.find((i) => i.type === metaTypes.results);
 
     if (hasCategory) {
-      const vo = valueArray.find((i) => i.type === 'category') as ValueObject;
+      const vo = valueArray.find((i) => i.type === metaTypes.category) as ValueObject;
       const val = vo.value;
       setCategory(val);
       const catObj = filterOptions.find((c) => c.category === val) as CategoryObject;
       const subCatgs = catObj.subcategories;
       if (subCatgs) {
-        setSelectOptions('subcategory', subCatgs);
+        setSelectOptions(metaTypes.subcategory, subCatgs);
+        setSubcategories(subCatgs);
       } else {
         setSelectOptions('', []);
+        setSubcategories([]);
       }
     } else {
-      setSelectOptions('category', []);
+      setSelectOptions(metaTypes.category, []);
+      setCategory('');
+      setSubcategories([]);
     }
 
     if (hasSubcategory) {
       setSelectOptions('', []);
-      const subCtgObj = valueArray.find((i) => i.type === 'subcategory') as ValueObject;
+      const subCtgObj = valueArray.find((i) => i.type === metaTypes.subcategory) as ValueObject;
       setSubcategory(subCtgObj.value);
+    } else {
+      setSubcategory('');
+    }
+
+    if (hasResults) {
+      const resultObj = valueArray.find((i) => i.type === metaTypes.results) as ValueObject;
+      notification.info({
+        message: `searched for \n category- ${category}, \n subcategory- ${subcategory}, \n query- ${resultObj.value}`,
+        duration: 5,
+      });
     }
   };
 
@@ -107,45 +133,52 @@ const AdvancedSearch = ({
     setSearchQuery(value);
     if (value) {
       const defaultSearch = {
-        value: `results-${value}`,
+        type: metaTypes.results,
+        value,
         label: <>{value}</>,
       };
-      setOptions([defaultSearch]);
+      const rData: OptionType[] = [];
+      rData.push(defaultSearch);
+      setOptions([{
+        label: '',
+        options: [defaultSearch],
+      }]);
+    } else if (category && (subcategory || subcategories.length === 0)) {
+      setSelectOptions('', []);
+    } else if (category) {
+      setSelectOptions(metaTypes.subcategory, subcategories);
     } else {
-      setSelectOptions('category', []);
+      setSelectOptions(metaTypes.category, []);
     }
   };
 
-  const handleClear = () => {
-    setSelectOptions('category', []);
-  };
-
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log(`${category}-${subcategory}-${searchQuery}`);
-  }, [searchQuery]);
-
-  useEffect(() => {
-    setSelectOptions('category', []);
+    setSelectOptions(metaTypes.category, []);
   }, []);
 
   return (
+    <div className="advanced-search-box">
+      <div className="search-button-container">
+        <Button
+          className="search-button"
+          onClick={() => setIsSearchModalOpen(true)}
+        >
+          <div className="search-button-internal-div">
+            Search in Concert Curation
+            <SearchOutlined />
+          </div>
 
-    <Select
-      mode="multiple"
-      allowClear
-      style={{
-        background: '#444',
-        color: '#FFF',
-        width: 354,
-        marginRight: '70px',
-      }}
-      options={options}
-      onChange={handleChange}
-      onSearch={handleSearch}
-      onClear={handleClear}
-    />
+        </Button>
+      </div>
 
+      <SearchPopup
+        isModalOpen={isSearchModalOpen}
+        setIsModalOpen={setIsSearchModalOpen}
+        options={options}
+        onTagChange={onTagChange}
+        handleSearch={handleSearch}
+      />
+    </div>
   );
 };
 
