9dcad4061a801e2b5f0ea53a73d64f0074cf23ee
praveen <praveen.prajapati.rebase>
2022-03-11 18:35:17 +0530

    if not approved stop loading and dont allow other than profile page and home page

---
+++ src/visualLayer/pages/profile/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+
+const createProfilePage = () => {
+  return function ProfilePage(): JSX.Element {
+    return <div style={{ margin: 'auto' }}>Profile page</div>;
+  };
+};
+
+export default createProfilePage;
