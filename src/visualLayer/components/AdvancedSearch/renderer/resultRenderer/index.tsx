33386ba9759e0290d73a7d78db4e41e7b4ed21dc
saritChauhan <saritsinghchauhan@gmail.com>
2022-03-22 10:53:47 +0530

    used search id coming from backend

--- src/visualLayer/components/AdvancedSearch/renderer/resultRenderer/index.tsx
+++ src/visualLayer/components/AdvancedSearch/renderer/resultRenderer/index.tsx
@@ -5,17 +5,20 @@ import { ReactNode } from 'react';
 import './styles.scss';
 
 type ResultRendererReturn={
+    id: string,
     type: string;
     value: string;
     label: ReactNode;
 }
 
 const ResultRenderer = (
+  id: string,
   title: string,
   type: string,
   image: string,
   description: string,
 ): ResultRendererReturn => ({
+  id,
   type,
   value: title,
   label: (
