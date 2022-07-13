d34b83ebead14549eb296b009135e6e2da26cc5b
sarit-rebase <sarit.chauhan.rebase@gmail.com>
2022-07-13 17:56:48 +0530

    fixed type error

--- src/visualLayer/components/ArtistProfile/artist-graphs/index.tsx
+++ src/visualLayer/components/ArtistProfile/artist-graphs/index.tsx
@@ -2,7 +2,7 @@
 import { CompareData } from '@rebaseitlabs/compare-data';
 import * as functionMappper from './functionMapper';
 
-const ArtistGraphs = ({ data, metaData }: any): any => {
+const ArtistGraphs = ({ data, metaData }: {data: any, metaData: any}): any => {
   const renderData = new CompareData(functionMappper);
   if (!data) return (<div>No graphs to show</div>);
   if (!metaData) return (<div>No graphs to show</div>);
